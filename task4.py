b953e33d2edcc100cc859d8cdffa437be2c4634b
Ptr-31 <myagkikh2001@yandex.ru>
2023-06-05 00:08:01 +0300

    caesar cipher

---
+++ task4.py
@@ -0,0 +1,22 @@
+#
+# Implement a function that takes a string as input and returns a new string where each alphabetic character is
+# shifted to the right by a given amount. For example, if the input string is "abc" and the shift amount is 2,
+# the output string should be "cde". If the shift amount is greater than 26, the function should wrap around to
+# the beginning of the alphabet.
+# Try to use modulus operand.
+
+def shift_letters(letters: str, step: int):
+    arr = [i for i in letters]
+    for i, symbol in enumerate(arr):
+        if (ord(symbol) in range(ord('a'), ord('z')+1)):
+            arr[i] = chr(((ord(symbol)) - ord('a') + step) % 26 + ord('a'))
+        elif(ord(symbol) in range(ord('A'), ord('Z')+1)):
+            arr[i] = chr(((ord(symbol)) - ord('A') + step) % 26 + ord('A'))
+    return "".join(arr)
+
+def main():
+    str = 'xyz'
+    print(shift_letters(str, 3))
+
+if __name__ == "__main__":
+    main()
