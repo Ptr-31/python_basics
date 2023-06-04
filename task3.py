c765a1fd1f9a7bff833d69ff28fe79b6a3b7c714
Ptr-31 <myagkikh2001@yandex.ru>
2023-06-04 19:13:54 +0300

    task3

---
+++ task3.py
@@ -0,0 +1,30 @@
+#
+# Task 3:
+# Implement a function that takes a string as input and returns a new string, containing only numbers.
+# Each number represents the position of the character in the alphabet. Ignore non alpha symbols like spaces,
+# puntuation, etc. If the char is lower than 10 append 0.
+# Do the solution with O(n) complexity.
+#
+# >>> alphabet_position("Hello, World!")
+# '0805121215, 2315181204!'
+
+def translate(string):
+    string = string.lower()
+    new_str = ''
+    for chr in string:
+        if chr in alphabet:
+            if alphabet[chr] < 9:
+                new_str += '0' + str(alphabet[chr])
+            else:
+                new_str += str(alphabet[chr])
+        else:
+            new_str += chr
+    return new_str
+alphabet = {l: i+1 for i, l in enumerate(list(map(chr, range(ord('a'),ord('z')))))} #yes, i'm doing dict instead of using list, yes I'm strange))
+
+def main():
+    string = "Hello, World!"
+    print(translate(string))
+
+if __name__ == "__main__":
+    main()
