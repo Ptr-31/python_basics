2364b6a6d3e3940770692899c1870a56e277cb57
Ptr-31 <myagkikh2001@yandex.ru>
2023-06-05 16:39:19 +0300

    updated searching algorithm

--- task4.py
+++ task4.py
@@ -7,15 +7,17 @@
 
 def shift_letters(letters: str, step: int):
     arr = [i for i in letters]
+    low_case_num_arr = [i for i in range(ord('a'), ord('z')+1)]
+    upper_case_num_arr = [i for i in range(ord('A'), ord('Z')+1)]
     for i, symbol in enumerate(arr):
-        if (ord(symbol) in range(ord('a'), ord('z')+1)):
+        if (ord(symbol) in low_case_num_arr):
             arr[i] = chr(((ord(symbol)) - ord('a') + step) % 26 + ord('a'))
-        elif(ord(symbol) in range(ord('A'), ord('Z')+1)):
+        elif(ord(symbol) in upper_case_num_arr):
             arr[i] = chr(((ord(symbol)) - ord('A') + step) % 26 + ord('A'))
     return "".join(arr)
 
 def main():
-    str = 'xyz'
+    str = 'XyZ'
     print(shift_letters(str, 3))
 
 if __name__ == "__main__":
