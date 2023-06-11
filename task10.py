92cb6eb1ac36e4b1cea44192214446e5a7dfc038
Ptr-31 <myagkikh2001@yandex.ru>
2023-06-11 15:15:24 +0300

    added type annotation

--- task10.py
+++ task10.py
@@ -7,8 +7,8 @@
 # >>> min_max_values([1, 3, 6, 5, 3, 2, 1, 5, -1, 5, 2, 10, 4, 3])
 # (-1, 10)
 
-def min_max_values(list_of_values: list[int]):
-    return ((min(list_of_values),max(list_of_values)))
+def min_max_values(list_of_values: list[int]) -> tuple((min, max)):
+    return ((min(list_of_values), max(list_of_values)))
 
 def main():
     print(min_max_values([1, 3, 6, 5, 3, 2, 1, 5, -1, 5, 2, 10, 4, 3]))
