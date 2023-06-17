0eb505912bbba55bfd5edce0906beff161adea4c
Ptr-31 <myagkikh2001@yandex.ru>
2023-06-18 00:46:49 +0300

    json_editor

---
+++ task13.py
@@ -0,0 +1,4 @@
+# You are given a JSON file containing data about employees in a company. And another JSON file containign
+# salaries for the different departments. Write a Python function that reads this file, parses the JSON data,
+# modifies it by adding a new key-value pair to each employee dictionary, and stores another json file a list
+# of dictionaries containing information about each employee with the added key-value pair.
